220675abab17c1d2162a1985e839d91f1555d66b
Ryan Dioneda <er.dioneda@gmail.com>
2024-08-30 08:27:52 -0400

    Added dashboard route

--- backend/main/main_routes.py
+++ backend/main/main_routes.py
@@ -5,3 +5,17 @@ main_bp = Blueprint("main_bp", __name__, template_folder="../../frontend/public/
 @main_bp.route('/')
 def index():
     return render_template('index.html')
+
+
+
+@main_bp.route('/dashboard')
+def dashboard():
+    
+    if 'access_token' in session:
+        access_token = session['access_token']
+        display_name = session.get('display_name', 'User')
+        profile_image = session.get('profile_image', '/static/default-profile.png')
+        return render_template('dashboard.html', access_token=access_token, display_name=display_name, profile_image=profile_image)
+        
+    else:
+        return redirect(url_for('index'))
